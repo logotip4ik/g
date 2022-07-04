f4c777ca14e63621504774f99538ec75c2a762ab
Bogdan Kostyuk (laptop) <bogdankostyuk12@gmail.com>
2022-07-04 23:30:56 +0300

    test: added new tests for new parser

--- tests/utils.test.js
+++ tests/utils.test.js
@@ -6,21 +6,49 @@ export function testParseArgs() {
   const argsResults = new Map([
     [
       "_ _ fix -m added some fixes -ph".split(" "),
-      ["fix", { m: " added some fixes", ph: true }],
+      {
+        commandType: "update",
+        commitType: "fix",
+        options: { m: " added some fixes", ph: true },
+      },
     ],
     [
-      "_ _ -m added some fixes -ph -pl".split(" "),
-      ["update", { m: " added some fixes", ph: true, pl: true }],
+      "_ _ chore! -m added some fixes -ph -pl".split(" "),
+      {
+        commandType: "update",
+        commitType: "chore!",
+        options: { m: " added some fixes", ph: true, pl: true },
+      },
     ],
-    ["_ _ sync".split(" "), ["sync", {}]],
-    ["_ _ push".split(" "), ["push", {}]],
-    ["_ _ pull".split(" "), ["pull", {}]],
+    [
+      "_ _ build(npm) -m added some fixes -ph -pl".split(" "),
+      {
+        commandType: "update",
+        commitType: "build(npm)",
+        options: { m: " added some fixes", ph: true, pl: true },
+      },
+    ],
+    [
+      "_ _ sync".split(" "),
+      { commandType: "sync", commitType: "", options: {} },
+    ],
+    [
+      "_ _ push".split(" "),
+      { commandType: "push", commitType: "", options: {} },
+    ],
+    [
+      "_ _ pull".split(" "),
+      { commandType: "pull", commitType: "", options: {} },
+    ],
   ]);
 
   argsResults.forEach((result, args) => {
-    const [command, options] = parseArgs(args);
+    const { commandType, commitType, options } = parseArgs(args);
 
-    assert.equal(command, result[0]);
-    assert.deepStrictEqual(options, result[1]);
+    console.log(commandType, commitType, options);
+
+    assert.equal(commandType, result.commandType);
+    assert.deepStrictEqual(commitType, result.commitType);
+    assert.deepStrictEqual(options, result.options);
   });
 }
